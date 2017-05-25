b70c925461a161be70567f85cc5d1149603b309f
Aaron VonderHaar <av@noredink.com>
2017-05-25 09:10:52 -0700

    Update to latest version of elm-test

--- tests/Example.elm
+++ tests/Example.elm
@@ -19,9 +19,9 @@ tests =
                     2 + 2
             in
                 expect expression to equal 4
-        , it "compares two numbers" <|
+        , it "compares two numbers (toBe true)" <|
             expect (10 > 5) toBe true "math should work"
-        , it "compares two numbers" <|
+        , it "compares two numbers (greaterThan)" <|
             expect 10 toBe greaterThan 5
         , fuzz (list int) "ends up with the same list when reversing twice" <|
             \fuzzList ->
